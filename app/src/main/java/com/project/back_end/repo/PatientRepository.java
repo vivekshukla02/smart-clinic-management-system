5ef47bfc6ef5fa144628fef82f6049c7258de387
Vivek Shukla <vivekshukla0552@gmail.com>
2026-06-23 23:29:58 +0530

    Create PatientRepository.java

---
+++ app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -0,0 +1,16 @@
+package com.project.back_end.repo;
+
+import com.project.back_end.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Patient findByEmail(String email);
+
+    Patient findByEmailOrPhoneNumber(
+            String email,
+            String phoneNumber
+    );
+}
